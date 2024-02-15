f0a7438745ca22660cbdd58d7a5ac5f301dc6f36
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2024-02-15 20:09:57 +0000

    add comments

--- client.c
+++ client.c
@@ -12,96 +12,121 @@
 
 #include "minitalk.h"
 
-static char	*g_temp;
+static char *g_temp;
 
-int	send_bit(int bit, int pid)
+// Function to send a single bit using signals
+int send_bit(int bit, int pid)
 {
-	static int	bit_position;
+    static int bit_position;
 
-	if (bit_position == 8)
-		bit_position = 0;
-	if (bit & (1 << bit_position++))
-	{
-		if (kill(pid, SIGUSR2) == -1)
-		{
-			ft_printf("Error\n");
-			free(g_temp);
-			exit(1);
-		}
-	}
-	else
-	{
-		if (kill(pid, SIGUSR1) == -1)
-		{
-			ft_printf("Error\n");
-			free(g_temp);
-			exit(1);
-		}
-	}
-	return (bit_position);
+    // Reset bit_position after 8 bits
+    if (bit_position == 8)
+        bit_position = 0;
+
+    // Send SIGUSR2 if the bit is 1, SIGUSR1 if the bit is 0
+    if (bit & (1 << bit_position++))
+    {
+        if (kill(pid, SIGUSR2) == -1)
+        {
+            perror("Error");
+            free(g_temp);
+            exit(1);
+        }
+    }
+    else
+    {
+        if (kill(pid, SIGUSR1) == -1)
+        {
+            perror("Error");
+            free(g_temp);
+            exit(1);
+        }
+    }
+    return bit_position;
 }
 
-void	send_message(int pid, char *msg)
+// Function to send a message using signals
+void send_message(int pid, char *msg)
 {
-	static int		stored_pid;
-	static size_t	i;
-	static size_t	len;
-	size_t			index_bit;
+    static int stored_pid;
+    static size_t i;
+    static size_t len;
+    size_t index_bit;
 
-	index_bit = 0;
-	if (stored_pid == 0)
-		stored_pid = pid;
-	if (!g_temp)
-		g_temp = ft_strdup(msg);
-	if (len == 0)
-		len = ft_strlen(g_temp);
-	if (i < len)
-		index_bit = send_bit(g_temp[i], stored_pid);
-	else if (i == len)
-		index_bit = send_bit('\0', stored_pid);
-	if (index_bit == 8)
-		i++;
-	if (index_bit == 8 && g_temp && i == len)
-		free(g_temp);
-	return ;
+    index_bit = 0;
+
+    // Store pid on first call
+    if (stored_pid == 0)
+        stored_pid = pid;
+    if (!g_temp)
+        g_temp = strdup(msg);
+    if (len == 0)
+        len = strlen(g_temp);
+
+    // Send each bit of the message
+    if (i < len)
+        index_bit = send_bit(g_temp[i], stored_pid);
+    else if (i == len)
+        index_bit = send_bit('\0', stored_pid);
+
+    // Increment index after sending 8 bits
+    if (index_bit == 8)
+        i++;
+
+    // Free the message when all bits are sent
+    if (index_bit == 8 && g_temp && i == len)
+        free(g_temp);
+
+    return;
 }
 
-void	handler(int signal, siginfo_t *info, void *context)
+// Signal handler function
+void handler(int signal, siginfo_t *info, void *context)
 {
-	(void)info;
-	(void)context;
-	if (signal == SIGUSR1)
-	{
-		ft_printf("Success!\n");
-		exit(0);
-	}
-	else if (signal == SIGUSR2)
-	{
-		send_message(0, NULL);
-	}
+    (void)info;
+    (void)context;
+    if (signal == SIGUSR1)
+    {
+        printf("Success!\n");
+        exit(0);
+    }
+    else if (signal == SIGUSR2)
+    {
+        send_message(0, NULL);
+    }
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	struct sigaction	sa_signal;
-	sigset_t			block_mask;
+    struct sigaction sa_signal;
+    sigset_t block_mask;
 
-	sigemptyset(&block_mask);
-	sa_signal.sa_flags = 0;
-	sa_signal.sa_mask = block_mask;
-	sa_signal.sa_sigaction = handler;
-	if (argc == 3 && argv[2][0] != '\0')
-	{
-		sigaction(SIGUSR1, &sa_signal, NULL);
-		sigaction(SIGUSR2, &sa_signal, NULL);
-		send_message(ft_atoi(argv[1]), argv[2]);
-		while (1)
-			sleep(1);
-		return (1);
-	}
-	else
-	{
-		ft_printf("Error: incorrect number of arguments.\n");
-		return (1);
-	}
+    // Initialize block_mask
+    sigemptyset(&block_mask);
+
+    // Configure signal handler
+    sa_signal.sa_flags = 0;
+    sa_signal.sa_mask = block_mask;
+    sa_signal.sa_sigaction = handler;
+
+    if (argc == 3 && argv[2][0] != '\0')
+    {
+        // Register signal handlers for SIGUSR1 and SIGUSR2
+        sigaction(SIGUSR1, &sa_signal, NULL);
+        sigaction(SIGUSR2, &sa_signal, NULL);
+
+        // Send the message
+        send_message(atoi(argv[1]), argv[2]);
+
+        // Infinite loop to keep the process running
+        while (1)
+            sleep(1);
+
+        return 1;
+    }
+    else
+    {
+        printf("Error: incorrect number of arguments.\n");
+        return 1;
+    }
 }
--- server.c
+++ server.c
@@ -12,99 +12,134 @@
 
 #include "minitalk.h"
 
-unsigned char	*add_byte_to_str(unsigned char *str, unsigned char c,
-		size_t len)
+// Function to add a byte to a string
+unsigned char *add_byte_to_str(unsigned char *str, unsigned char c, size_t len)
 {
-	unsigned char	*temp;
-	int				i;
+    unsigned char *temp;
+    int i;
 
-	temp = (unsigned char *)ft_strdup((char *)str);
-	free(str);
-	if (!temp)
-		exit(1);
-	str = malloc(sizeof(unsigned char) * (len + 2));
-	if (!str)
-	{
-		free(temp);
-		exit(1);
-	}
-	ft_bzero(str, len + 2);
-	i = -1;
-	while (temp[++i])
-		str[i] = temp[i];
-	str[len] = c;
-	str[len + 1] = '\0';
-	if (temp)
-		free(temp);
-	return (str);
+    // Duplicate the existing string
+    temp = (unsigned char *)ft_strdup((char *)str);
+    free(str);
+    if (!temp)
+        exit(1);
+
+    // Allocate memory for the new string
+    str = malloc(sizeof(unsigned char) * (len + 2));
+    if (!str)
+    {
+        free(temp);
+        exit(1);
+    }
+    ft_bzero(str, len + 2);
+    // Copy the content from the duplicated string to the new string
+    i = -1;
+    while (temp[++i])
+        str[i] = temp[i];
+    // Add the new byte to the string
+    str[len] = c;
+    str[len + 1] = '\0';
+    if (temp)
+        free(temp);
+
+    return str;
 }
 
-int	add_bits_to_byte(unsigned char received_byte, size_t len, int send_pid)
+// Function to add received bits to a byte and handle message completion
+int add_bits_to_byte(unsigned char received_byte, size_t len, int send_pid)
 {
-	static unsigned char	*str;
+    static unsigned char *str;
 
-	if (received_byte == '\0' && str)
-	{
-		if (kill(send_pid, SIGUSR1) == -1)
-		{
-			free(str);
-			exit(1);
-		}
-		ft_printf("\n%s", str);
-		free(str);
-		str = NULL;
-		return (1);
-	}
-	if (!str)
-	{
-		str = malloc(sizeof(unsigned char) * 2);
-		if (!str)
-			exit(1);
-		str[0] = received_byte;
-		str[1] = '\0';
-	}
-	else
-		str = add_byte_to_str(str, received_byte, len);
-	return (0);
+    if (received_byte == '\0' && str)
+    {
+        // Send SIGUSR1 to acknowledge the end of the message
+        if (kill(send_pid, SIGUSR1) == -1)
+        {
+            free(str);
+            exit(1);
+        }
+        // Print the received message and free the string
+        ft_printf("\n%s", str);
+        free(str);
+        str = NULL;
+        return 1;
+    }
+    // Allocate memory for the initial byte if the string is empty
+    if (!str)
+    {
+        str = malloc(sizeof(unsigned char) * 2);
+        if (!str)
+            exit(1);
+
+        // Initialize the string with the received byte
+        str[0] = received_byte;
+        str[1] = '\0';
+    }
+    else
+    {
+        // Add the received byte to the string
+        str = add_byte_to_str(str, received_byte, len);
+    }
+
+    return 0;
 }
 
-void	handler(int signal, siginfo_t *info, void *context)
+// Signal handler function
+void handler(int signal, siginfo_t *info, void *context)
 {
-	static unsigned char	bits;
-	static int				bit_position;
-	static int				pid;
-	static size_t			len;
+    static unsigned char bits;
+    static int bit_position;
+    static int pid;
+    static size_t len;
 
-	(void)context;
-	if (pid != info->si_pid)
-		pid = info->si_pid;
-	if (signal == SIGUSR2)
-		bits |= (1 << bit_position);
-	if (bit_position++ == 7)
-	{
-		if (add_bits_to_byte(bits, len++, pid) == 1)
-			len = 0;
-		bits = 0;
-		bit_position = 0;
-	}
-	kill(pid, SIGUSR2);
+    (void)context;
+
+    // Update the process ID if it changes
+    if (pid != info->si_pid)
+        pid = info->si_pid;
+
+    // Process received bits when SIGUSR2 is received
+    if (signal == SIGUSR2)
+        bits |= (1 << bit_position);
+
+    // Check if a full byte has been received
+    if (bit_position++ == 7)
+    {
+        // Add the received bits to the byte and handle message completion
+        if (add_bits_to_byte(bits, len++, pid) == 1)
+            len = 0;
+
+        // Reset variables for the next byte
+        bits = 0;
+        bit_position = 0;
+    }
+
+    // Send a SIGUSR2 to acknowledge the received bit
+    kill(pid, SIGUSR2);
 }
 
-int	main(void)
+int main(void)
 {
-	struct sigaction	sa_signal;
-	sigset_t			block_mask;
+    struct sigaction sa_signal;
+    sigset_t block_mask;
 
-	sigemptyset(&block_mask);
-	sigaddset(&block_mask, SIGINT);
-	sigaddset(&block_mask, SIGQUIT);
-	ft_printf("Server PID: %i\nWaiting for message.... \n", getpid());
-	sa_signal.sa_flags = SA_SIGINFO;
-	sa_signal.sa_mask = block_mask;
-	sa_signal.sa_sigaction = handler;
-	sigaction(SIGUSR1, &sa_signal, NULL);
-	sigaction(SIGUSR2, &sa_signal, NULL);
-	while (1)
-		pause();
-	return (0);
+    // Initialize the block_mask
+    sigemptyset(&block_mask);
+    sigaddset(&block_mask, SIGINT);
+    sigaddset(&block_mask, SIGQUIT);
+
+    ft_printf("Server PID: %i\nWaiting for message.... \n", getpid());
+
+    // Configure signal handler for SIGUSR1 and SIGUSR2
+    sa_signal.sa_flags = SA_SIGINFO;
+    sa_signal.sa_mask = block_mask;
+    sa_signal.sa_sigaction = handler;
+    sigaction(SIGUSR1, &sa_signal, NULL);
+    sigaction(SIGUSR2, &sa_signal, NULL);
+
+    // Infinite loop to wait for signals
+    while (1)
+        pause();
+
+    return 0;
 }
